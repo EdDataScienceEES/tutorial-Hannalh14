72825ac07fb96dd100a2dc90c6604468b6caf33e
Your GitHub Username <your_email@example.com>
2022-11-27 18:34:16 +0000

    new data

--- Tutorial_script .R
+++ Tutorial_script .R
@@ -6,15 +6,26 @@
 #Install needed packages 
 install.packages("tidyverse")
 install.packages("treemap")
-
+install.packages("dplyr")
 #Load needed packages 
 library(tidyverse)
 library(treemap)
+library(dplyr)
 
 #Set working directory to where you saved your files for this tutorial 
 setwd("~/Desktop/EdDataScience/tutorial-Hannalh14/script")
 
 #Load dataset 
-dinodata <- read.csv("data.csv")
-view(dinodata)
+popdata <- read.csv("us_pop_by_state.csv") 
+
+view(popdata)
+
+#data minipulation 
+
+#Make basic plot 
+dinotm <- treemap(dinodata,
+                  index = c("lived_in"),
+                  vSize = "length",
+                  type= "index")
+
 
