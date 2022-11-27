562af8129bb6706d0573c85ad84758046a68c728
Your GitHub Username <your_email@example.com>
2022-11-27 13:58:49 +0000

    install packages and load data

--- Tutorial_script .R
+++ Tutorial_script .R
@@ -2,3 +2,18 @@
 #Created by: Hanna Lord-Howe 
 #Date: December 2nd, 2022
 #EdDataScienceEES
+
+#Install needed packages 
+install.packages("tidyverse")
+install.packages("treemap")
+
+#Load needed packages 
+library(tidyverse)
+library(treemap)
+
+#Set working directory to where you saved your files for this tutorial 
+setwd("~/Desktop/EdDataScience/tutorial-Hannalh14/script")
+
+#Load dataset 
+dinodata <- read.csv("data.csv")
+view(dinodata)
