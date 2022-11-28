523dd1fd8d476c2cc9f87aa521aa8349e23d9f19
Your GitHub Username <your_email@example.com>
2022-11-28 16:55:14 +0000

    tried wes anderson and added title

--- Tutorial_script .R
+++ Tutorial_script .R
@@ -8,11 +8,13 @@ install.packages("tidyverse")
 install.packages("treemap")
 install.packages("dplyr")
 install.packages("readr")
+install.packages("wesanderson")
 #Load needed packages 
 library(tidyverse)
 library(treemap)
 library(dplyr)
 library(readr)
+library(wesanderson)
 
 #Set working directory to where you saved your files for this tutorial 
 setwd("~/Desktop/EdDataScience/tutorial-Hannalh14/script")
@@ -36,7 +38,13 @@ abvtm <- treemap(statepop,
                  vSize = "X2020_census",
                  type = "index")
 
-
+#Add different elements to the plot
+##Adding labels 
+abvtm <- treemap(statepop,
+                 index = c("state_code"),
+                 vSize = "X2020_census",
+                 type = "index",
+                 title = "United States Population by State")
 
 
 
